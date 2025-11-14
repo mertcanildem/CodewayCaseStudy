5798327b2862ce44b289c9293d32892ddcb72ccc
mertildem <mertcanildem9@gmail.com>
2025-11-14 15:19:13 +0300

    Initial Commit

---
+++ CodewayCaseStudy/CodewayCaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodewayCaseStudyApp.swift
+//  CodewayCaseStudy
+//
+//  Created by Mert can Ildem on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodewayCaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
